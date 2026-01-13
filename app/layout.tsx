e11db71e201624711fff748bd7067c5009f222cd
MGalaCyber <ibmanggalaputra1274@gmail.com>
2026-01-13 21:23:44 +0700

    Fix font not loaded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type React from "react";
 
+import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Site } from "@/lib/config";
 import "./globals.css";
+
+const _geist = Geist({ subsets: ["latin"] });
+const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: Site.Home.title,
